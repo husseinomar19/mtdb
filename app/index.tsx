08e5f0d8cb425867fa66ee0e10beae4ada1b2738
Hussein <omerhussen603@gmail.com>
2025-04-23 12:53:59 +0200

    feat: implement user registration with Firebase authentication and add error handling

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,23 @@
-import { Link } from "expo-router";
-import { SafeAreaView, Text, View, Image, ActivityIndicator, TextInput, Pressable, ScrollView ,Platform } from "react-native";
-
-
+import { Link, router } from "expo-router";
+import { SafeAreaView, Text, View, Image, Pressable, TextInput} from "react-native";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {provider , auth} from "../firebase"
+import { useState } from "react";
 
 export default function Index() {
-
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const register = async () => {
+        try {
+          await createUserWithEmailAndPassword(auth, email, password);
+          router.replace("/(tabs)/home");
+        } catch (e) {
+          setError("Fout bij registreren: " + e);
+          console.log(e);
+        }
+      };
 
 
   return (
@@ -19,7 +32,33 @@ export default function Index() {
 
             <Text className="text-white text-[20px] mt-2">Welcome to the Movie Database</Text>
             
-            
+    <View style={{ padding: 20 }} className="w-full ">
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        style={{ marginBottom: 10 }}
+        className="bg-white rounded-[50px] pt-4 pb-4 pr-11 pl-4 w-full"
+      />
+      <TextInput
+        placeholder="Wachtwoord"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={{ marginBottom: 10 }}
+        className="bg-white rounded-[50px] pt-4 pb-4 pr-11 pl-4 w-full"
+      />
+      <Pressable className="bg-white pt-4 pb-4 pr-11 pl-4 rounded-[50px] text-center" onPress={register}>
+        <Text className="text-center font-medium">Inloggen</Text>
+      </Pressable>
+      {error ? <Text style={{ color: "red" }}>{error}</Text> : null}
+    </View>
+
+
+           {error && (
+            <Text className="text-red-500 text-[16px] mt-2">{error}</Text>
+            )}
       </View>
     </SafeAreaView>
   );
